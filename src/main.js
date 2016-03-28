2ab1a96ab4fb2d1346234b21f9ad4923f09ad52b
Joe-noh <honzawa.j@pepabo.com>
2016-03-28 20:25:02 +0900

    pipes are coming

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ let game = new Phaser.Game(400, 490);
 let mainState = {
   preload() {
     game.load.image('bird', 'assets/images/bird.png');
+    game.load.image('pipe', 'assets/images/pipe.png');
   },
 
   create() {
@@ -13,8 +14,12 @@ let mainState = {
     game.physics.arcade.enable(this.bird);
     this.bird.body.gravity.y = 1000;
 
+    this.pipes = game.add.group();
+
     let spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
+
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
   },
 
   update() {
@@ -24,14 +29,35 @@ let mainState = {
   },
 
   jump() {
-      this.bird.body.velocity.y = -350;
+    this.bird.body.velocity.y = -350;
   },
 
   restartGame() {
-      game.state.start('main');
+    game.state.start('main');
+  },
+
+  addOnePipe(x, y) {
+    let pipe = game.add.sprite(x, y, 'pipe');
+    this.pipes.add(pipe);
+
+    game.physics.arcade.enable(pipe);
+
+    pipe.body.velocity.x = -200;
+
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+
+  addRowOfPipes() {
+    let hole = Math.floor(Math.random() * 5) + 1;
+
+    for (let i=0; i < 8; i++) {
+      if (i != hole && i != hole + 1) {
+        this.addOnePipe(400, i * 60 + 10);
+      }
+    }
   }
 };
-
 
 game.state.add('main', mainState);
 game.state.start('main');
